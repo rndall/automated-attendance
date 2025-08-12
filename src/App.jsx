efc0ba0edd36baea15281f432099ed432baf657e
rndall <96964503+rndall@users.noreply.github.com>
2025-08-12 16:10:14 +0800

    feat: import components to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import "@mantine/core/styles.css";
 
 import { MantineProvider } from "@mantine/core";
+import { Box, Group } from "@mantine/core";
+
+import QRScanner from "./components/QRScanner";
+import Details from "./components/Details";
 
 function App() {
-  return <MantineProvider>{/* Your app here */}</MantineProvider>;
+  return (
+    <MantineProvider>
+      <Group grow>
+        <Box>
+          <QRScanner />
+        </Box>
+
+        <Box>
+          <Details />
+        </Box>
+      </Group>
+    </MantineProvider>
+  );
 }
 
 export default App;
