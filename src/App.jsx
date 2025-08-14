9bd855cf50171eb158dff79b152dac07f4c3453e
rndall <96964503+rndall@users.noreply.github.com>
2025-08-14 08:51:42 +0800

    refactor: separate MantineProvider children into a separate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,13 @@
 import "@mantine/core/styles.css";
 
-import { useDisclosure } from "@mantine/hooks";
 import { MantineProvider } from "@mantine/core";
-import { Box, Flex, Stack } from "@mantine/core";
 
-import QRScanner from "./components/QRScanner";
-import Details from "./components/Details";
-import SuccessModal from "./components/SuccessModal";
-
-import { useState } from "react";
+import AttendanceSection from "./components/AttendanceSection";
 
 function App() {
-  const defaultStyles = {
-    padding: 10,
-    borderRadius: 10,
-    border: "1px solid #BBB",
-  };
-
-  const [scannedStudentId, setScannedStudentId] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [opened, { open, close }] = useDisclosure(false);
-
-  const handleScan = (result) => {
-    setScannedStudentId(result);
-
-    // Fetch student details
-
-    console.log(result);
-  };
-
-  const handleCancelAttendance = () => setScannedStudentId(null);
-
-  const handleSaveAttendance = async () => {
-    setLoading(true);
-
-    // Mock attendance saving api
-    setTimeout(() => {
-      open();
-      setScannedStudentId(null);
-      setLoading(false);
-    }, 500);
-  };
-
   return (
     <MantineProvider>
-      <Stack bg="#e4e4e4" mih="100dvh" justify="center">
-        <Flex
-          py={{ base: defaultStyles.padding, xl: 5 }}
-          px={defaultStyles.padding}
-          gap={20}
-          direction={{ base: "column", md: "row" }}
-        >
-          <Box
-            bg="white"
-            flex={1}
-            p={defaultStyles.padding}
-            bdrs={defaultStyles.borderRadius}
-            bd={defaultStyles.border}
-            miw={0}
-          >
-            <QRScanner onScan={handleScan} />
-          </Box>
-
-          <Box
-            bg="white"
-            flex={1}
-            p={defaultStyles.padding}
-            bdrs={defaultStyles.borderRadius}
-            bd={defaultStyles.border}
-            miw={0}
-          >
-            <Details
-              onSaveAttendance={handleSaveAttendance}
-              onCancelAttendance={handleCancelAttendance}
-              hasScanned={!!scannedStudentId}
-              loading={loading}
-            />
-          </Box>
-        </Flex>
-      </Stack>
-
-      <SuccessModal opened={opened} close={close} />
+      <AttendanceSection />
     </MantineProvider>
   );
 }
---
+++ src/components/AttendanceSection.jsx
@@ -0,0 +1,85 @@
+import { Box, Flex, Stack } from "@mantine/core";
+
+import QRScanner from "./QRScanner";
+import Details from "./Details";
+import SuccessModal from "./SuccessModal";
+
+import { useState } from "react";
+import { useDisclosure } from "@mantine/hooks";
+
+const defaultStyles = {
+  padding: 10,
+  borderRadius: 10,
+  border: "1px solid #BBB",
+};
+
+function AttendanceSection() {
+  const [scannedStudentId, setScannedStudentId] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [opened, { open, close }] = useDisclosure(false);
+
+  const handleScan = (result) => {
+    setScannedStudentId(result);
+
+    // Fetch student details
+
+    console.log(result);
+  };
+
+  const handleCancelAttendance = () => setScannedStudentId(null);
+
+  const handleSaveAttendance = async () => {
+    setLoading(true);
+
+    // Mock attendance saving api
+    setTimeout(() => {
+      open();
+      setScannedStudentId(null);
+      setLoading(false);
+    }, 500);
+  };
+
+  return (
+    <>
+      <Stack bg="#e4e4e4" mih="100dvh" justify="center">
+        <Flex
+          py={{ base: defaultStyles.padding, xl: 5 }}
+          px={defaultStyles.padding}
+          gap={20}
+          direction={{ base: "column", md: "row" }}
+        >
+          <Box
+            bg="white"
+            flex={1}
+            p={defaultStyles.padding}
+            bdrs={defaultStyles.borderRadius}
+            bd={defaultStyles.border}
+            miw={0}
+          >
+            <QRScanner onScan={handleScan} />
+          </Box>
+
+          <Box
+            bg="white"
+            flex={1}
+            p={defaultStyles.padding}
+            bdrs={defaultStyles.borderRadius}
+            bd={defaultStyles.border}
+            miw={0}
+          >
+            <Details
+              onSaveAttendance={handleSaveAttendance}
+              onCancelAttendance={handleCancelAttendance}
+              hasScanned={!!scannedStudentId}
+              loading={loading}
+            />
+          </Box>
+        </Flex>
+      </Stack>
+
+      <SuccessModal opened={opened} close={close} />
+    </>
+  );
+}
+
+export default AttendanceSection;
